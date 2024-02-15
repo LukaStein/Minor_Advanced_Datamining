8e465a2023cbd7bb0faf2a5fa576d06ddf3fe410
Coldbirdie <60214213+Coldbirdie@users.noreply.github.com>
2024-02-15 15:40:38 +0100

    Working basic perceptron.

--- AdvancedDataMining/H1/model.py
+++ AdvancedDataMining/H1/model.py
@@ -2,7 +2,7 @@ class Perceptron():
         def __init__(self, dim):
               self.dim = dim
               self.bias = 0
-              self.weights = 0
+              self.weights = [0, 0]
 
         def __repr__(self):
             text = f'Perceptron(dim={self.dim})'
@@ -14,31 +14,32 @@ class Perceptron():
             param: geneste lijst van lijsten
             return: enkelvoudige lijst
             """
-            predictions = [] 
+            predictions_yhat = [] 
             for xCoords in xs:
                 x1 = xCoords[0]
                 x2 = xCoords[1]
                 # initiële voorspelling
-                sgn = self.bias + (self.weights * x1) + (self.weights * x2)
-                predict = lambda x : -1.0 if x < 0  else (0.0 if x == 0 else 1.0)
-                yhat = predict(sgn)
+                yValue = self.bias + (self.weights[0] * x1) + (self.weights[1] * x2)
+                predictLabel = lambda x : -1.0 if x < 0  else (0.0 if x == 0 else 1.0)
+                yhat = predictLabel(yValue)
                 # bewaar voorspellingen 
-                predictions.append(yhat)
-            return predictions
+                predictions_yhat.append(yhat)
+            return predictions_yhat
         
         def partial_fit(self, xs, ys):
-             yhat = self.predict(xs)
-             print(yhat)
-             ys_yhat = [ys, yhat]
-             print(ys_yhat)
-             for x,y in zip(xs,ys):
-                  print(x, y)
+             yhat : list = self.predict(xs)
+             index = 0
+             for x,yOld in zip(xs,ys):
+                #   print(x, y, yhat[index])
                   # update-regel
-                # biasUpdate = self.bias - (yhat - sgn)
-                # weight1 = self.weights - (yhat - sgn) * x1
-                # weight2 = self.weights - (yhat - sgn) * x2
-                # # opnieuw voorspellen
+                  self.bias = self.bias - (yhat[index] - yOld)
+                  self.weights[0] = self.weights[0] - (yhat[index] - yOld) * x[0]
+                  self.weights[1] = self.weights[1] - (yhat[index] - yOld) * x[1]
+                  # opnieuw voorspellen
+                  yNew = self.bias + (self.weights[0] * x[0]) + (self.weights[1] * x[1])
+                  predictLabel = lambda x : -1.0 if x < 0  else (0.0 if x == 0 else 1.0)
+                  predictLabel(yNew)
+                  index += 1
+            #  print(yhatNewPredictions)
                 # #print(sgn, biasUpdate, weight1, x1, weight2, x2)
-                # sgnUpdate = biasUpdate + (weight1 * x1) + (weight2 * x2)
-                # yhatTraining = predict(sgnUpdate)
             
