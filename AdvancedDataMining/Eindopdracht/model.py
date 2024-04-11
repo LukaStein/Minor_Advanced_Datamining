d455cdb4b42bd6f5e98e911249bf9d6050974f34
Coldbirdie <60214213+Coldbirdie@users.noreply.github.com>
2024-04-11 23:03:18 +0200

    Finished chapter 5 and fixed an error in binary CE

--- AdvancedDataMining/Eindopdracht/model.py
+++ AdvancedDataMining/Eindopdracht/model.py
@@ -167,14 +167,10 @@ def categorical_crossentropy(yhat_no, y_no, e=0.01): # loss
      return -y_no * (log(e) + (yhat_no - e) / e)  # else take log of e instead of yhat_no
      
 def binary_crossentropy(yhat_no, y_no, e=0.01): # loss
-     logE = (log(e) + (yhat_no - e / e))
-     logYhat, logYhat2 = logE, logE
      if yhat_no >= e: # if not to close to zero
-          logYhat = log(yhat_no)
-     if 1 - yhat_no >= e:
-          logYhat2 = log(1 - yhat_no)
-     return -y_no * logYhat - (1 - y_no) * logYhat2 # else take log of e instead of yhat_no
-     # (log(e) + (yhat_no - e / e))
+          return -y_no * log(yhat_no) - (1 - y_no) * log(yhat_no)
+     return -y_no * (log(e) + ((yhat_no - e) / e)) - (1 - y_no) * (log(e) + (yhat_no - e) / e) # else take log of e instead of yhat_no
+     
      
 def derivative(function, delta=0.8):
      
